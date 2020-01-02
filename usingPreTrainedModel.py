e7c3581046ad28303d59f2283b528049207633c5
Eric Marcantonio <eric11marcantonio@gmail.com>
2020-01-01 23:03:03 -0500

    API

--- usingPreTrainedModel.py
+++ usingPreTrainedModel.py
@@ -1,28 +1,29 @@
 import pickle
-from os import listdir
 
 from keras.models import load_model
+from keras.optimizers import Adam
 from numpy import expand_dims
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import Normalizer
-from sklearn.svm import SVC
-from loadDataSetForTraining import returnForModelTraining, extract_face, get_embedding
 
+from loadDataSetForTraining import extract_face, get_embedding
 
 # Holds model
-model = None
+
 # Loading model
 with open("trained_model", "rb") as f:
-    model = pickle.load(f) 
+    model = pickle.load(f)
 
-out_encoder = None
+# Load the encoder
 with open("out_encoder", "rb") as f:
     out_encoder = pickle.load(f)
 
-for filename in listdir("test_pics"):
-    print(filename)
 
-    oneEmb = get_embedding(load_model('facenet_keras.h5', compile=False), extract_face("test_pics/" + filename))
+def getPrediction(filename):
+    '''Compile and load the facenet embedding'''
+    MODELK = load_model('facenet_keras.h5')
+    optimizer = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    MODELK.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    oneEmb = get_embedding(MODELK, extract_face("test_pics/" + filename))
 
     samples = expand_dims(oneEmb, axis=0)
     yhat_class = model.predict(samples)
@@ -34,4 +35,4 @@ for filename in listdir("test_pics"):
     predict_names = out_encoder.inverse_transform(yhat_class)
 
     print(predict_names[0])
-    print(class_prob)
+    print(round(class_prob, 5))
