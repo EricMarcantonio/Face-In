53f79366543da82d5e1f5e4f16ee24c6c6ad3862
M-Faheem-Khan <faheem5948@gmail.com>
2020-01-02 19:21:24 -0500

    Created mongodb api + setup some flask routes

---
+++ flask/db.py
@@ -0,0 +1,62 @@
+import pymongo
+from bson import objectid
+
+# Connection String
+class Mongodb(object):
+    # INIT FUNCTION
+    def __init__(self, connectionString, db):
+        print("Attempting to Connect with database")
+        try:
+            self.connection = pymongo.MongoClient(connectionString) # initiating connection with database
+            self.db = self.connection[db]
+            print("Connected with database")
+        except Exception as e:
+            print(e)
+
+    # APPENDs Document to collection
+    def Append(self, collection, params):
+        try:
+
+            _id = self.db[collection].insert_one(params)
+            print("ID: %s"%(objectid.ObjectId(_id.inserted_id)))
+            return {
+                "response": True,
+                "_id": str(objectid.ObjectId(_id.inserted_id))
+            }
+
+        except Exception as e:
+            print(e)
+            return False
+
+    # UPDATEs Documents in collection
+    def Update(self, collection, id, params):
+        try:
+            self.db[collection].find_one_and_update({"_id", objectid.ObjectId(id)}, params)
+            print("database updated")
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    # LOGs all information
+    def Log(self, path, method, params, time):
+        try:
+            self.db["Logs"].insert_one({"path": path, "method": method, "params": params, "time": time })
+            print("Request Logged")
+        except Exception as e:
+            print(e)
+    
+    def ReadSingle(self, docID):
+        data = self.db["Users"].find({"_id": objectid.ObjectId(docID)}, {"_id":1, "name":1, "RegisterationStatus":1, "images":1})
+        x = data.next()
+        x["_id"] = str(objectid.ObjectId(x["_id"]))
+        return {
+            "response": True,
+            "data": x
+        }
+
+    def ReadAll(self):
+        return {
+            "response": True,
+            "data":self.db["Users"].find({})
+        }
--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,12 @@
 import base64 # for base64 decoding 
-from flask import Flask, render_template, request # flask related 
+from flask import Flask, render_template, request, jsonify # flask related 
+from db import Mongodb
+from datetime import datetime
 
 app = Flask(__name__)
+
+# Initating connection with mongodb
+dbClient = Mongodb("mongodb://localhost:27017/", "Personal")
 
 # DEFAULT ROUTE
 @app.route("/", methods=["GET"])
@@ -12,15 +17,83 @@ def home():
 # {
     # image: "base-64-encoded-image-data"
 # }
-@app.route("/image", methods=["GET", "POST"])
+@app.route("/valuation", methods=["POST"])
 def getImage():
     if (request.method == "POST"): # checking if request is post
-        image = request.get_json(force=True)["image"][22:] # removing descriptor for the data data:image/png;base64,
-        with open("test_image.png", "wb") as f: # writing image to file in binary
-            f.write(base64.b64decode(image)) # decoding image from base64
-    
-    return "" # depending on the response return a response 
+        image = request.get_json(force=True)["image"]
+        image = image[image.index(",")+1:] # removing descriptor for the data data:image/png;base64,
+        dbClient.Log("/valuation", "POST", {"image": image}, datetime.now()) # api request logs        
+        params = {
+            "path": "/valuation",
+            "method": "POST",
+            "image": [image],
+            "time": datetime.now()
+        }
+        dbClient.Append("Testing", params) # api requests for valuation
+        return "YES or NO depending on model response" # depending on the response return a response 
+    else:
+        return "Invalid Request Method"
+
+
+@app.route("/admin/home", methods=["GET"])
+def adminHome():
+    return "RETURN ADMIN HOME"
+
+
+# Batch Upload user photos
+@app.route("/admin/user/add", methods=["POST"])
+def adminUpload():
+    if (request.method == "POST"):
+        name = request.get_json(force=True)["name"] # getting users name
+        images = request.get_json(force=True)["images"] # getting array of images associated with the user
+        registeration_status = request.get_json(force=True)["RegisterationStatus"]
+        
+        # removing image descriptors
+        # for i in range(len(images)):
+        #     images[i] = images[i][images[i].index(",")+1:] # removing descriptor for the data data:image/png;base64,
+
+        # User information
+        params = {
+            "name": name,
+            "images": images,
+            "RegisterationStatus": registeration_status
+        }
+        # saving users information to collection
+        repsonse = dbClient.Append("Users", params) # returns True/False depending on if the user document was created
+
+        # api request logging information object
+        params = {
+            "images": images,
+            "name": name,
+            "RegisterationStatus": registeration_status
+        }
+        dbClient.Log("/admin/users/add", "POST", params, datetime.now())
+        return jsonify(repsonse) # api request logs
+    else:
+        return "Invalid Request Method"
+
+@app.route("/admin/user/update", methods=["POST"])
+def adminUpdateUsers():
+    if (request.method == "POST"):
+        userId = request.get_json(force=True)["userId"] # docId to update
+        params = request.get_json(force=True)["update"] # object values to update
+        # api request logging information object
+        dbClient.Log("/admin/user/update", "POST", params, datetime.now()) # api request logs
+        return dbClient.Update("Users", userId, params) # returns True/False depending on if the document was updated
+    else:
+        return "Invalid Request Method"
+
+@app.route("/personal/user/<userId>", methods=["GET"])
+def getUser(userId):
+    # if user id equal -1 return all users from database
+    if (request.method == "GET"):
+        if (userId != -1):
+            return dbClient.ReadSingle(userId)
+        else:
+            return jsonify(dbClient.ReadAll())
+    else:
+        return "Invalid Request Method"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host="0.0.0.0")
-
